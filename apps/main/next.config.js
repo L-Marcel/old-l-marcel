6d4d47e81c99b3b45e4036475d289bed50e2c20a
LMarcel <lmgh1312@gmail.com>
2021-12-17 22:30:34 -0300

    Removendo Rewrites

--- apps/main/next.config.js
+++ apps/main/next.config.js
@@ -5,15 +5,7 @@ module.exports = {
       {
         source: '/:path*',
         destination: `/:path*`,
-      },
-      {
-        source: '/example',
-        destination: `l-marcel.vercel.app/example`,
-      },
-      {
-        source: '/example',
-        destination: `l-marcel.vercel.app/example/:path*`,
-      },
+      }
     ]
   },
 }
