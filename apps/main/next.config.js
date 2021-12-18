4691baf7f3153ec7d4d05e24bf74541e197a83dc
LMarcel <lmgh1312@gmail.com>
2021-12-17 22:50:54 -0300

    Adicionando Rewrites

--- apps/main/next.config.js
+++ apps/main/next.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: `/:path*`,
+      },
+      {
+        source: '/example',
+        destination: `l-marcel-example.vercel.app/example`,
+      },
+      {
+        source: '/example',
+        destination: `l-marcel.-example.vercel.app/example/:path*`,
+      },
+    ]
+  },
 }
