c52343a539010e4d539a076c957e3228fed04db8
LMarcel <lmgh1312@gmail.com>
2022-02-06 22:33:58 -0300

    Listagem de projetos

--- apps/projects/src/pages/index.tsx
+++ apps/projects/src/pages/index.tsx
@@ -1,6 +1,32 @@
+import Link from "next/link";
+import style from "../styles/index.module.scss";
+
 function Home() {
+  const projects: Project[] = [
+    {
+      name: "Snake",
+      image: "",
+      support: ["desktop"]
+    }
+  ];
+
   return (
-    <h1>Hello World</h1>
+    <div
+      className={style.container}
+    >
+      <h1>Projetos hospedados:</h1>
+      <div className={style.list}>
+        {projects.map(p => {
+          return (
+            <Link href={`/${p.name.toLocaleLowerCase().replace(/ /g, "-")}`} key={p.name}>
+              <div className={style.item}>
+                <h1>{p.name}</h1>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
   );
 };
 
